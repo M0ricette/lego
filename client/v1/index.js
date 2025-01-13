0658b724d0d0fb1a50694cc97a8f48a612de202b
simon@gmail.com <simon@gmail.com>
2025-01-13 20:11:56 +0100

    TODO2

--- client/v1/index.js
+++ client/v1/index.js
@@ -66,6 +66,9 @@ console.log(nb_of_deals);
 // 2. Log the variable
 // 3. Log how many shopping communities we have
 
+
+
+
 // 🎯 TODO 4: Sort by price
 // 1. Create a function to sort the deals by price
 // 2. Create a variable and assign it the list of sets by price from lowest to highest
